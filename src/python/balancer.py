78d660b528ee8a3ec9ee0307f28e477e7e5bb250
Romilly Cocking <romilly.cocking@gmail.com>
2018-03-02 13:48:55 +0000

    Calculates molecular weight

--- src/python/balancer.py
+++ src/python/balancer.py
@@ -1,30 +1,33 @@
 import re
 from collections import defaultdict
+from aw import edict
 
-# becomes = re.compile('->')
 expr_form = re.compile('(\((?:[A-Z][a-z]?[\d]*)*\)[\d]*|[A-Z][a-z]?[\d]*)')
 compound_form = re.compile('([A-Z][a-z]?)([\d]*)')
 bracketed_form = re.compile('\(((?:[A-Z]|[a-z]|[\d])*)\)([\d]*)')
 
+def count(string):
+    return int(string) if string else 1
+
+def amu(element):
+    return edict[element][2]
+
 def atoms_in(compound):
     dict = defaultdict(int)
     for expr in expr_form.findall(compound):
-        print(expr)
         if expr.startswith('('):
             m = bracketed_form.match(expr)
             chain = m.group(1)
-            factor = int(m.group(2)) if m.group(2) else 1
+            factor = count(m.group(2))
             d = atoms_in(chain)
             for key in d.keys():
                 dict[key] += factor * d[key]
         else:
             for (element, qty) in compound_form.findall(expr):
-                dict[element] += int(qty) if qty else 1
+                dict[element] += count(qty)
     return dict
 
-# (lhs, rhs) = becomes.split('Fe2O3+SO2->CuO3+H2O+CO2')
-# dod = {}
-# for item in items:
-#     dod[item] = atoms_in(item)
-# print(dod)
-# print (lhs, rhs)
+def molecular_weight(compound):
+    quantities = atoms_in(compound)
+    return sum([amu(element) * quantities[element] for element in quantities.keys()])
+
